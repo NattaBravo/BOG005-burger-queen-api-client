8d5ca02dac89c8be7ba0359d80a9dc228ba45c27
yenymag22 <yenyespinosa25@gmail.com>
2022-12-01 13:13:35 -0500

    prueba 2 yeny

--- burger-queen/src/index.js
+++ burger-queen/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>"hello word Nata"</h1>
+root.render(<h1>"hello word Nata como estas"</h1>
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
