afe35bba14994bb6271f0e62f19950317a0efe04
NattaBravo <fiido.project@gmail.com>
2023-03-27 00:18:25 -0500

    Continuidad en la maquetación de la vista del mesero, ajuste para movil

---
+++ burger-queen/src/Components/header.js
@@ -0,0 +1,15 @@
+import logoBQ from '../Resources/LogoBQ.png';
+
+const HeaderBQ = () => {
+
+    return (
+        <>
+            <header className='mainHeader'>
+                <img src={logoBQ} alt="" className='mainLogo'></img>
+            </header>
+        </>
+    )
+
+}
+
+export default HeaderBQ;
--- burger-queen/src/Pages/Waiter/WaiterUI.js
+++ burger-queen/src/Pages/Waiter/WaiterUI.js
@@ -1,54 +1,65 @@
-import logoBQ from '../../Resources/LogoAjustado.png';
-import breakfast from '../../Resources/Desayuno.jpg'
+//import breakfast from '../../Resources/Desayuno.jpg'
+import HeaderBQ from "../../Components/header"
 
 function WaiterUI() {
-      
-    return (
-      <>
 
-        <header className='mainHeader'>
-          <img src={logoBQ} alt="" className='mainLogo'></img>
-        </header>
+  return (
+    <>
 
-        <section>
+      <section>
+        < HeaderBQ />
+      </section>
+
+      <section className='waiterBody'>
+
+        <section className="wellcome">
           <h1>
-            ¡Hola, Natalia!
+            ¡HOLA, NATALIA!
           </h1>
-          <p>Mesera</p>
-          <h2>
+          <h3>Mesera</h3>
+          <p>
             ¡Vamos a hacer de hoy un día delicioso!
-          </h2>
-
+          </p>
         </section>
-        <section className='optionForProduct'>
-          <article>
-            <div>
-              <img alt="" src={breakfast}className="imageForProduct"></img>
-            </div>
-            <div className='buttonForProduct'>
-              <p>perro</p>
-              
-            </div>
 
-
-
-          </article>
-          <article>
+        <h2>PRODUCTOS</h2>
+        <article className="breakfast">
+          <div className="breakfastImage"></div>
+          <div className="breakfastTitle">
+            <p className="breakfastPTitle1">DESA</p>
+            <p className="breakfastPTitle2">YUNO</p>
             
-          </article>
+          </div>
+        </article>
+        <article className="lunch">
+          <div className="lunchTitle">
+          <p className="lunchPTitle1">AL</p>
+            <p className="lunchPTitle2">MUER</p>
+            <p className="lunchPTitle3">ZO</p>
+          </div>
+          <div className="lunchImage"></div>
+        </article>
 
-
-
+        <h2>TABLE STATE</h2>
+        <section className="tableState">
+            <p className="tableStateTitle">Observa el mapa de tus mesas y su estado actual</p>
         </section>
 
+        <h2>ÓRDENES</h2>
+        <section className="clickToOrders">
+            <p className="ordersTitle">Sigue aquí todas las ordenes en tiempo real</p>
+        </section>
 
-       
-        <footer>
+        <footer className="mainFooter">
           <p>Made by Yeny and Natha</p>
         </footer>
 
-      </>
-    )
+
+      </section>
+      
+
+    </>
+  )
 }
 
 export { WaiterUI }
